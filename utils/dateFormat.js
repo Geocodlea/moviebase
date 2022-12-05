8a2d912c5033268181302455555b063fa386deb3
George Anton <geocodlea@yahoo.com>
2022-12-05 18:13:50 +0200

    small fix

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,4 +1,6 @@
 export default function dateFormat(date) {
+  if (!/\d{4}.\d{2}.\d{2}/.test(date)) return date;
+
   const day = date[8] + date[9];
   const month = date[5] + date[6];
   const year = date[0] + date[1] + date[2] + date[3];
